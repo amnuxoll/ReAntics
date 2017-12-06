ec165f2279a9ea6928eb627d2a4c7eeea109f187
Christian <rodrigch18@up.edu>
2017-12-06 10:55:14 -0800

    Fixed the unit tests in testStatsPane.py

--- Antics/Unit Tests/testStatsPane.py
+++ Antics/Unit Tests/testStatsPane.py
@@ -1,12 +1,21 @@
+import sys
+# this needs to be here for module importing with directory change
+# windows
+sys.path.append(".\\")
+# unix
+sys.path.append("./")
 import os
+# some code assumes the CWD, so we have to do a work around for unit tests
+# to keep unit tests in a separate folder
 os.chdir("..")
 import unittest
-import Game
+from Game import Game
 from GUIHandler import GUIHandler
+from GameState import GameState
+from Construction import Construction
+from Ant import Ant
 from Constants import *
-from Player import Player
-import re
-import StatsPane as sp
+
 
 
 class testStatsPane(unittest.TestCase):
